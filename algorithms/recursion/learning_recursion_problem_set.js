d493e0b958d8252f0e155fa89d30dbec060bd3d8
Javier Flores Apodaca <javier.floresapodaca@gmail.com>
2024-11-12 08:50:00 -0800

    started working on recursion part 2

--- algorithms/recursion/learning_recursion_problem_set.js
+++ algorithms/recursion/learning_recursion_problem_set.js
@@ -9,4 +9,36 @@ function factorial(num) {
     if (num === 2) return 2;
     return num * factorial(num - 1);
 }
-console.log(factorial(4));
+console.log(factorial(5));
+
+// Below are two ways to solve the same issue:
+// Helper Method Recursion
+function collectOddValuesHelperMethodRecursion(arr){
+    let result = [];
+    function helper(helperInput){
+        if(helperInput.length === 0) return;
+        if(helperInput[0] % 2 !== 0) result.push(helperInput[0])
+        helper(helperInput.slice(1))
+    }
+    helper(arr)
+    return result;
+}
+console.log(collectOddValuesHelperMethodRecursion([1,2,3,4,5,6,7,8,9]));
+// Pure Recursion
+function collectOddValuesPureRecursion(arr){
+    let newArr = [];
+    if(arr.length === 0) return newArr;
+    if(arr[0] % 2 !== 0) newArr.push(arr[0]);
+    newArr = newArr.concat(collectOddValuesPureRecursion(arr.slice(1)));
+    return newArr;
+}
+console.log(collectOddValuesPureRecursion([1,2,3,4,5,6,7,8,9]));
+
+function productOfArray(nums) {
+    if (!nums.length) {
+        return 1;
+    }
+    return nums.pop() * productOfArray(nums);
+}
+
+console.log('product of Array: ', productOfArray([1,2,3,10]));
