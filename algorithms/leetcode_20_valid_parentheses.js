5902632787ad6e1bbc9a81327bf93ac376ace5ea
Javier Flores Apodaca <javier.floresapodaca@gmail.com>
2024-11-11 20:37:21 -0800

    Solved problem 20 leetcode

--- algorithms/leetcode_20_valid_parentheses.js
+++ algorithms/leetcode_20_valid_parentheses.js
@@ -1,7 +1,19 @@
 const Stack = require('../data-structures/Stack');
-
-// Time Complexity = O(N) - We only have to iterate of 's' once
-// Space Complexity = O(N) - As 's' increases, so does 'charStack'
+/**
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ *
+ * An input string is valid if:
+ *
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ *
+ * Time Complexity = O(N) - We only have to iterate of 's' once
+ * Space Complexity = O(N) - As 's' increases, so does 'charStack'
+ *
+ * @param s
+ * @returns {boolean}
+ */
 let isValid = function(s) {
     let charStack = new Stack();
     let pairDictionary = {
